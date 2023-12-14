6f45526b6ea726cd4944cb988ab83101981814bf
Sean Smith <SSmith@winklevoss.com>
2023-12-14 14:51:25 -0500

    second commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TestPublic;
+
+namespace TestCPC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestCPC test = new TestCPC();
+            var s = test.SqRt(22);
+            Console.WriteLine("SquareRoot of 22=" + s);
+            Console.ReadLine();
+        }
+    }
+}
